1000a8bcf3c6b7690f75f26c214d1721306052c7
Marfn Mlakha <mlakham@umich.edu>
2024-06-21 12:27:28 -0400

    Learned if statements and deleted duplicate math

--- basics/if_statements/js/main.js
+++ basics/if_statements/js/main.js
@@ -1,1 +1,29 @@
-console.log('Hello World!');
+// Conditionals: If Statements
+
+// Syntax
+if (false) {
+    // code block
+} else if (true) {
+    // code block
+} else {
+    // code block
+}
+
+// Example
+let age = 18;
+if (age >= 18 && isNaN(age) == false) {
+    console.log('You are an adult');
+} else if (age < 18 && isNaN(age) == false){
+    console.log('You are a minor');
+} else {
+    console.log('Invalid age');
+}
+
+age = "not a number";
+if (age >= 18 && isNaN(age) == false) {
+    console.log('You are an adult');
+} else if (age < 18 && isNaN(age) == false){
+    console.log('You are a minor');
+} else {
+    console.log('Invalid age');
+}
