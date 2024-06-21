742c4163b3954a365d25c2ea355b03d655effb36
Marfn Mlakha <mlakham@umich.edu>
2024-06-21 12:39:56 -0400

    Chained ternary operators

--- basics/ternary operators/js/main.js
+++ basics/ternary operators/js/main.js
@@ -5,3 +5,8 @@ let expression = true;
 let result = expression ? 'this is true' : 'this is false';
 
 console.log(result);
+
+// chained ternary operators
+let day = "wednesday";
+let isWeekend = day === "saturday" || day === "sunday" ? "weekend" : "weekday";
+console.log(isWeekend);
