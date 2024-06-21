69247df11d9b57abec9f9807e4c3751444d1add8
Marfn Mlakha <mlakham@umich.edu>
2024-06-21 13:11:43 -0400

    user input

--- basics/user_input/js/main.js
+++ basics/user_input/js/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+// User Input
+
+// alets the user with a message
+alert("Hello World");
+
+// confirm prompts the user with a message and returns a boolean value
+var userResult = confirm("Are you sure you want to do this?");
+console.log(userResult);
+
+// prompt prompts the user with a message and returns the user input
+var userInput = prompt("What is your name?");
+console.log(userInput);
