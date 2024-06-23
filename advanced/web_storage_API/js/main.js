b223e2137af51a45be2b1aa4a39373e09c0cda3f
Marfn Mlakha <mlakham@umich.edu>
2024-06-23 13:29:21 -0400

    Learned Web Storage

--- advanced/web_storage_API/js/main.js
+++ advanced/web_storage_API/js/main.js
@@ -1,1 +1,39 @@
-console.log('Hello World!');
+// Web Storage API
+
+// Not part of the DOM -- refers to the Window API
+// Avilable to JS via the global object 'window'
+
+// window.alert("Hello World!");
+// alert("Hello World!");
+
+console.log(window.location);
+
+// #region Local Storage
+
+const myObject = {
+    name: "John",
+    age: 30,
+    city: "New York",
+
+    toString: function () {
+        return `${this.name} is ${this.age} years old and lives in ${this.city}`;
+    },
+};
+
+const myArray = ["eat", "sleep", "code", "repeat"];
+
+myObject.toString(); // this is not saved in local storage
+
+// Store data in local storage as a string
+localStorage.setItem("person", myObject);
+localStorage.setItem("tasks", myArray);
+const mySessionData = localStorage.getItem("person");
+const mySessionTasks = localStorage.getItem("tasks");
+
+// retrive the local storage data
+console.log(mySessionData);
+console.log(mySessionTasks);
+
+// we can retrive this data after leaving the website
+
+// #endregion
