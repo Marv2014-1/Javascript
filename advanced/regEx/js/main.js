469c1e653f0ba6449cde492e48f8ac8366a80472
Marfn Mlakha <mlakham@umich.edu>
2024-06-23 17:51:01 -0400

    Learned regex

--- advanced/regEx/js/main.js
+++ advanced/regEx/js/main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+// regular expression to validate email
+
+// function to validate email
+function validateEmail(email) {
+    // regular expression
+    var regEx =
+        /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
+    return regEx.test(email);
+}
+
+// testing the function
+var email = "email@email.com";
+console.log(validateEmail(email)); // true
